9c92deb9d7dfffad7f98941fb804b7969a794e98
Anubhav Poddar <anubhavpoddar1@gmail.com>
2026-07-14 03:18:10 +0530

    Time: 0 ms (100%), Space: 52.9 MB (58.09%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int l=0, r=nums.length-1;
+        int tar=-1;
+
+        while(l<r)
+        {
+            int m=(l+r)/2;
+
+            if(m-1 > 0 && m+1<nums.length-1)
+            {
+                if(m+1 < nums.length && m-1 >=0 && nums[m]!=nums[m+1] && nums[m-1]!= nums[m])
+                return nums[m];
+            
+                if(nums[l]!=nums[l+1]) return nums[l];
+                if(nums[r]!=nums[r-1]) return nums[r];
+            
+                if(nums[m]==nums[m-1] )
+            {
+                if((m-l+1)%2 != 0 )
+                r=m-2;
+                else l=m+1;
+            }
+
+            else if(nums[m]==nums[m+1] )
+            {
+                if((r-m+1)%2 != 0)
+                l=m+2;
+                else r=m-1;
+            }
+            }
+            
+            else
+            {
+                break;
+            }
+        }
+        if(nums.length == 1) return nums[0];
+        
+        int m=(l+r)/2;
+        if(nums[l]!=nums[l+1]) return nums[l];
+        if(nums[r]!=nums[r-1]) return nums[r];
+        if(nums[m] != nums[m+1] && nums[m]!=nums[m-1])
+        return nums[m];
+
+        return tar;
+    }
+}
