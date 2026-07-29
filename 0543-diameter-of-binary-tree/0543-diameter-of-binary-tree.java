dcb5e6625087bd7e15e7966053bc02ab659afa9c
Anubhav Poddar <anubhavpoddar1@gmail.com>
2026-07-30 00:46:47 +0530

    Time: 1 ms (45.84%), Space: 47.3 MB (16.43%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -15,31 +15,19 @@
  */
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
-        int llen=0, rlen=0, max=0;
-        Stack<TreeNode> st =new Stack<>();
-        st.push(root);
-        while(!st.empty())
-        {
-            TreeNode tp=st.pop();
-        llen=treelen(tp.left);
-        rlen=treelen(tp.right);
-        max=Math.max(max, llen+rlen);
-        if(tp.left != null)
-        st.push(tp.left);
-        if(tp.right != null)
-        st.push(tp.right);
-        }
-
-        return max;
+        int max[]=new int[1];
+        treelen(root, max);
+        return max[0];
     }
-    int treelen(TreeNode lroot)
+    int treelen(TreeNode root, int []max)
     {
-        int max=0;
-        if(lroot == null) return 0;
-        max=treelen(lroot.left);
-        max=Math.max(max, treelen(lroot.right));
+        if(root == null) return 0;
+        
+        int llen=treelen(root.left,max);
+        int rlen=treelen(root.right,max);
+        max[0]=Math.max(max[0], llen+rlen);
 
-        return max+1;
+        return 1+Math.max(llen, rlen);
     }
     
 }
