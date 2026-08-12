6600b74a92439c84875f947d4d4aa17ce976a34e
Anubhav Poddar <anubhavpoddar1@gmail.com>
2026-08-12 16:55:43 +0530

    Time: 1 ms (69.75%), Space: 43.5 MB (69.64%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -17,8 +17,7 @@ class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> ans=new ArrayList<>();
         if(root == null) return ans;
-        
-        TreeMap<Integer, ArrayList<Integer>> map=new TreeMap<>();
+
         Queue<TreeNode> q=new LinkedList<>();
         q.offer(root);
         int lvl=1;        
@@ -26,23 +25,20 @@ class Solution {
         while(!q.isEmpty())
         {
             int s=q.size();
-            map.putIfAbsent(lvl, new ArrayList<>());
+             ArrayList<Integer> lt= new ArrayList<>();
             for(int i=1;i<=s;i++){
                 TreeNode tp=q.poll();
 
-                map.get(lvl).add(tp.val);
+                lt.add(tp.val);
 
                 if(tp.left != null)
                 q.offer(tp.left);
                 if(tp.right != null)
                 q.offer(tp.right);
             }
+            int a=lt.get(lt.size()-1);
+            ans.add(a);
             lvl++;
-        }
-        for(ArrayList<Integer> a:map.values())
-        {
-            int l=a.size();
-            ans.add(a.get(l-1));
         }
         
         return ans;
