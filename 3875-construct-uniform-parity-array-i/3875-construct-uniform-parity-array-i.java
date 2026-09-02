44c827137ebe62b217145bfca58231c51241795e
Anubhav Poddar <anubhavpoddar1@gmail.com>
2026-09-02 13:13:42 +0530

    Time: 1 ms (31.89%), Space: 44.9 MB (87.6%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int l=nums1.length;
+        int[] n2=new int[nums1.length];
+        int ce=0, co=0;
+
+        for(int i=0;i<l; i++)
+        {
+            if(nums1[i]%2 == 0)
+            {
+                ce++;
+                for(int j=0; j<l;j++)
+                {
+                    if(nums1[j]%2 !=0 && i!=j)
+                    {
+                        n2[i]=nums1[i]-nums1[j];
+                        break;
+                    }
+                }
+            }
+            else
+            {co++;
+            n2[i]=nums1[i];
+            }
+        }
+        if(co==l|| ce==l)
+        return true;
+        
+        int i=0;
+        while(i<l)
+        {
+            if( Math.abs(n2[i])%2 == 0)
+            break;
+            i++;
+        }
+        if(i<l)
+        return false;
+
+        return true;
+    }
+}
